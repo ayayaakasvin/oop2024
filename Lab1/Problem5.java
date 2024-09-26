c88fc4449c8aaf1c8c864f5312a3c713dee867be
Nurik <ayayaakasvin@gmail.com>
2024-09-26 20:30:56 +0500

    lab 1 add

--- Lab1/Problem5.java
+++ Lab1/Problem5.java
@@ -1,5 +1,7 @@
 package Lab1;
 
+import java.util.Vector;
+
 enum Gender {
     Male, Female
 }
@@ -13,5 +15,46 @@ class Person {
 }
 
 class Main {
-    private Vector<Person> toBeEat;  
+    private Vector<Person> toBeEat = new Vector<>();
+    
+    public void kidnap (Person p) {
+        toBeEat.add(p);
+    }
+
+    public boolean willDragonEatOrNot () {
+        for (int i = 0; i < toBeEat.size() - 1;) {
+            if (toBeEat.get(i).gender == Gender.Male && toBeEat.get(i + 1).gender == Gender.Female) {
+                toBeEat.removeElementAt(i);
+                toBeEat.removeElementAt(i);
+                if (i > 0) {
+                    i--;
+                }
+                continue;
+            }
+
+            i++;
+        }
+
+        return toBeEat.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Main some = new Main();
+        some.kidnap(new Person(Gender.Female));
+        some.kidnap(new Person(Gender.Male));
+        some.kidnap(new Person(Gender.Female));
+        some.kidnap(new Person(Gender.Male));
+
+        System.out.println(some.willDragonEatOrNot()); // false
+
+
+        some.toBeEat.clear(); // concerns about encapsulation, but for task ig its ok
+        // to clear we can just create another method, or create another object to use :)
+        some.kidnap(new Person(Gender.Male));
+        some.kidnap(new Person(Gender.Male));
+        some.kidnap(new Person(Gender.Female));
+        some.kidnap(new Person(Gender.Female));
+
+        System.out.println(some.willDragonEatOrNot()); // true
+    }
 }
